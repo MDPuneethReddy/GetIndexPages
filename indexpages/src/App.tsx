7135ad87287abe214f4f4bcc72a41223a5ee2ba2
puneeth <puneethmotukuru@gmail.com>
2021-05-24 14:56:29 +0530

    Add comment

--- indexpages/src/App.tsx
+++ indexpages/src/App.tsx
@@ -4,6 +4,7 @@ import { GetIndexex } from './components/GetIndex/GetIndexes';
 import 'antd/dist/antd.css';
 import ReactGa from "react-ga"
 function App() {
+  // For analytics
   useEffect(() => {
     ReactGa.initialize(process.env.REACT_APP_ANALYTICS_TOKEN as string)
     ReactGa.pageview(window.location.pathname+window.location.search)
