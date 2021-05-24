899994f7115ba4f17b6481c881282bd5d3be3ec5
puneeth <puneethmotukuru@gmail.com>
2021-05-24 14:30:45 +0530

    add token

--- indexpages/src/App.tsx
+++ indexpages/src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { GetIndexex } from './components/GetIndex/GetIndexes';
 import 'antd/dist/antd.css';
+import ReactGa from "react-ga"
 function App() {
+  useEffect(() => {
+    ReactGa.initialize(process.env.REACT_APP_ANALYTICS_TOKEN as string)
+    ReactGa.pageview(window.location.pathname+window.location.search)
+  }, [])
   return (
     <div className="App">
       <GetIndexex />
