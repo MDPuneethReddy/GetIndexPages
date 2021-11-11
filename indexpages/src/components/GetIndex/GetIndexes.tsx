bbdfef2f66d61191d1997238b3a9edede1e8ec65
MDPuneethReddy <puneethmotukuru@gmail.com>
2021-11-11 21:20:10 +0530

    Get data through sockets

--- indexpages/src/components/GetIndex/GetIndexes.tsx
+++ indexpages/src/components/GetIndex/GetIndexes.tsx
@@ -1,20 +1,18 @@
-import { Button, Input, message,Spin, Table } from "antd"
-import {FacebookOutlined,LinkedinOutlined} from "@ant-design/icons"
-import axios from "axios"
+import { Button, Input,Spin, Table } from "antd"
+import {LinkedinOutlined} from "@ant-design/icons"
 import React, { useState } from "react"
-import { getWithExpiry, setWithExpiry } from "../utils/localStorageExpiry"
 import { Helmet } from "react-helmet";
+import { io } from "socket.io-client";
 interface Iprops{
 
 }
 export const GetIndexex:React.FC<Iprops>=(props:Iprops)=>{
     const backendURL=process.env.REACT_APP_BACKEND_URL || "http://localhost:3333"
     const [url,setUrl]=useState<any>("")
-    const [data,setData]=useState<any>([])
+    const [data,setData]=useState<Array<any>>([])
     const [page, setPage] = useState<number>(1);
     const [disable,setDisable]=useState<boolean>(false)
     const [loading,setLoading]=useState<boolean>(false)
-    const localexpiry=21600000 
     const columns = [
         {
             title:"Sno",
@@ -27,78 +25,44 @@ export const GetIndexex:React.FC<Iprops>=(props:Iprops)=>{
           render: (text:any) => <a>{text}</a>,
         },
       ];
-   
-    const getData=async()=>{   
-            axios.get(`${backendURL}/api/getAllUrls`,{
-                headers:{
-                    url:url
-                }
-            }).then(response=>{
-                setData(response.data.payload)
-                setWithExpiry(url,response.data.payload,localexpiry)
-                setDisable(false)
-                setLoading(false)
-            }).catch(error=>{
-                message.error("something went wrong")
-                setDisable(false)
-                setLoading(false)
-            })
-    }
-    const getNoOfRecords=async()=>{
-        axios.get(`${backendURL}/api/getNoOfRecords`,{
-            headers:{
-                url:url
-            }
-        }).then(response=>{
-            message.success(`About ${Number(response.data.payload.split(',').join(''))} records`)
-        }).catch(error=>{
-            console.log(error)
-        })
-    }
-    const localStorageCheck=async ()=>{
-        if(url.trim()!==""){
-            // await getNoOfRecords()
-            message.success("fetching data......")
-            const value=getWithExpiry(url)
-            if(value===null){
-                await getData()
-            }else{
-                setData(value)
-                setDisable(false)
-                setLoading(false)
-            }    
-        }
-        else{
-            message.error("URL cannot be empty")  
-        }
+      const socket = io(backendURL, { transports: ["websocket"] });   
+      socket.on("url-links",(urls)=>{
+          console.log(urls)
+        setData((prevData:any)=>[...prevData,urls])
+      })
+      socket.on('disconnect', ()=> {
+        console.log("disconnected")
+        setDisable(false)
+        setLoading(false) 
+    });
+    const getData=async()=>{
+        socket.emit("indexpages-url",url)
     }
     const getAllIndexpages=async()=>{
         setDisable(true)
         setLoading(true)
-        await localStorageCheck()
+        await getData()
     }
     return(
-        <>
+        <div>
             <Helmet>
                 <meta charSet="utf-8" />
                 <title>Index Checker: Get Index pages of the website</title>
                 <meta name="description" content="Index Checker will helps you find out all the index pages of your blog or website easily with just your website name" />
             </Helmet>
-            <a style={{float:"right"}} href={process.env.REACT_APP_FACEBOOK}><FacebookOutlined  style={{ fontSize: '20px', color: '#08c' }}/></a>
             <a style={{float:"right"}} href={process.env.REACT_APP_LINKEDIN}><LinkedinOutlined  style={{ fontSize: '20px', color: '#08c' }}/></a>
             <h1 style={{textAlign:"center"}}><strong>Get Index pages of the website</strong></h1>
-            <label>Enter the URL :</label>
-            <Input placeholder="http or https://example.com" value={url} disabled={disable} onChange={(e:any)=>{
+            <label>Enter URL :  </label>
+            <Input style={{width:"70%"}} placeholder="http or https://example.com" value={url} disabled={disable} onChange={(e:any)=>{
                 setUrl(e.target.value)
-            }} />
+            }} /><br />
             <Button style={{textAlign:"center"}} type="primary" size="small" disabled={disable}  onClick={getAllIndexpages}>Click here !!</Button>
-            {loading?<Spin />:
-            <Table columns={columns} dataSource={data}  pagination={{
+            {loading&&<Spin />}
+            {/* <Table columns={columns} dataSource={data}  pagination={{
                 onChange(current:any) {
                 setPage(current);
                 }
-            }} />
-}
-        </>
+            }} /> */}
+        </div>
     )
 }
--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,8 @@ import cors from "cors"
 import puppeteer from 'puppeteer-extra';
 process.setMaxListeners(Infinity)
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-import {getAllLinks, getPage,getTotalRecords} from "./utils/utils"
+import {getAllUrls} from "./utils/utils"
+import {Server} from "socket.io"
 puppeteer.use(StealthPlugin())
 const app=express()
 app.use(cors())
@@ -12,53 +13,16 @@ const port=process.env.PORT||3333
 app.get("/api",(req,res)=>{
   res.send("Welcome to backend API")
 })
-app.get("/api/getNoOfRecords",async(req,res)=>{
-  const url=req.headers.url
-  const site=`site://${url}`
-  const {browser,page}=await getPage(req)
-  // enter site in google search
-  await page.type(".gLFyf.gsfi",site)
-  await page.keyboard.press('Enter')
-  // wait for page to load
-  await page.waitForSelector("#result-stats");
-  // await page.screenshot({path: 'screenshot.png'});
-  const text=await page.$eval("#result-stats",(e:any)=>e.textContent)
-  // get total records
-  const value=await getTotalRecords(text)
-  res.send({
-    message:"success",
-    payload:value
-  })
-})
-app.get("/api/getAllUrls",async(req,res)=>{
-  const url=req.headers.url
-  const site=`site://${url}`
-  let allLinks:any;
-  const {browser,page}=await getPage(req)
-  try{ 
-    await page.type(".gLFyf.gsfi",site)
-    await page.keyboard.press('Enter')
-    // await page.setRequestInterception(true);
-    await page.waitForSelector("#rso > div > div > div > div.yuRUbf > a");
-    allLinks = await page.$$eval("#rso > div > div > div > div.yuRUbf > a",( am:any) => am.filter((e:any) => e.href).map((e:any )=> e.href))
-    const results=await getAllLinks(page,allLinks)
-    res.send({
-    message:"success",
-    payload: results
-  })
-  await page.close();
-  await browser.close();
-  }
-catch(error){
-  console.log(error)
-  res.status(404).send("something went wrong")
-  await page.close();
-  await browser.close();
-  // console.log({browserKilled: await wasBrowserKilled(browser)});
-}
-})
 export const server=
   app.listen(port,async()=>{
   console.log(`listening on ${port}/api`)
 })
 server.on("error",console.error)
+const io = new Server(server);
+io.on('connection', socket => {
+  console.log('Some client connected')
+  socket.on("indexpages-url",(url:string)=>{
+    getAllUrls(url,socket)
+  })
+})
+
