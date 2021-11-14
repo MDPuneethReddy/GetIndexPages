7d1960a09d97296a93d64305c7ef7a1f2d3ccac5
MDPuneethReddy <puneethmotukuru@gmail.com>
2021-11-14 22:37:19 +0530

    Add Url Check

--- indexpages/src/components/GetIndex/GetIndexes.tsx
+++ indexpages/src/components/GetIndex/GetIndexes.tsx
@@ -1,6 +1,6 @@
 import { Button, Input,Spin, Table } from "antd"
 import {LinkedinOutlined} from "@ant-design/icons"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { Helmet } from "react-helmet";
 import { io } from "socket.io-client";
 interface Iprops{
@@ -13,6 +13,7 @@ export const GetIndexex:React.FC<Iprops>=(props:Iprops)=>{
     const [page, setPage] = useState<number>(1);
     const [disable,setDisable]=useState<boolean>(false)
     const [loading,setLoading]=useState<boolean>(false)
+    const [error,setError]=useState<boolean>(false)
     const columns = [
         {
             title:"Sno",
@@ -38,10 +39,25 @@ export const GetIndexex:React.FC<Iprops>=(props:Iprops)=>{
     const getData=async()=>{
         socket.emit("indexpages-url",url)
     }
+    const checkValidUrl=()=>{
+        if(url.match(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/)){
+            return true
+        }
+        else{
+            return false
+        }
+    }
     const getAllIndexpages=async()=>{
+        if(url.trim()!=='' && checkValidUrl()){
+        setData([])
         setDisable(true)
         setLoading(true)
         await getData()
+        }
+        else{
+            setError(true)
+            setDisable(true)
+        }
     }
     return(
         <div>
@@ -53,9 +69,12 @@ export const GetIndexex:React.FC<Iprops>=(props:Iprops)=>{
             <a style={{float:"right"}} href={process.env.REACT_APP_LINKEDIN}><LinkedinOutlined  style={{ fontSize: '20px', color: '#08c' }}/></a>
             <h1 style={{textAlign:"center"}}><strong>Get Index pages of the website</strong></h1>
             <label>Enter URL :  </label>
-            <Input style={{width:"70%"}} placeholder="http or https://example.com" value={url} disabled={disable} onChange={(e:any)=>{
+            <Input style={{width:"70%"}} placeholder="http or https://example.com" value={url} disabled={loading}  onChange={(e:any)=>{
+                setDisable(false)
+                setError(false)
                 setUrl(e.target.value)
             }} /><br />
+            {error && <p style={{color:"red"}}>* Invalid Url !</p>}
             <Button style={{textAlign:"center"}} type="primary" size="small" disabled={disable}  onClick={getAllIndexpages}>Click here !!</Button>
             {loading&&<Spin />}
             <div >
